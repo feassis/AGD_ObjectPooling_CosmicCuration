db3d3b7fde382f4e887f4f65ea99898fb70083ec
Felipe Assis <felipe.assis.2002@gmail.com>
2024-10-27 15:26:47 -0300

    Implementing generic pool get item method

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+        private List<PooledItem<T>> pool = new List<PooledItem<T>>();
+
+        protected T GetItem()
+        {
+            if(pool.Count > 0)
+            {
+                var item = pool.Find(i => i.IsUsed == false);
+
+                if(item != null)
+                {
+                    item.IsUsed = true;
+                    return item.Item;
+                }
+            }
+
+            return CreatePooledItem();
+        }
+
+        private T CreatePooledItem()
+        {
+            PooledItem<T> pooledItem = new PooledItem<T>();
+            pooledItem.Item = CreateItem();
+            pooledItem.IsUsed = true;
+            pool.Add(pooledItem);
+            return pooledItem.Item;
+        }
+
+        protected virtual T CreateItem()
+        {
+            throw new NotImplementedException("Chiled Not implemented Create Item");
+        }
+
+        public class PooledItem<T>
+        {
+            public T Item;
+            public bool IsUsed;
+        }
+    }
+}
+
