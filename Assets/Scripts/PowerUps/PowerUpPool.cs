3e561c94da87a837f54a0f586aaff5418efb313f
Felipe Assis <felipe.assis.2002@gmail.com>
2024-10-28 13:55:59 -0300

    creating power up pool

---
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -0,0 +1,34 @@
+﻿using CosmicCuration.Utilities;
+using System;
+
+namespace CosmicCuration.PowerUps
+{
+    public class PowerUpPool : GenericObjectPool<PowerUpController>
+    {
+        private PowerUpData powerUpData;
+
+        public PowerUpController GetPowerUpController<T>(PowerUpData powerUpData) where T : PowerUpController
+        {
+            this.powerUpData = powerUpData;
+            return GetItem<T>();
+        }
+
+        protected override PowerUpController CreateItem<T>()
+        {
+            if(typeof(T) == typeof(Shield))
+            {
+                return new Shield(powerUpData);
+            }
+            else if(typeof(T) == typeof(RapidFire))
+            {
+                return new RapidFire(powerUpData);
+            }
+            else if(typeof(T) == typeof(DoubleTurret))
+            {
+                return new DoubleTurret(powerUpData);
+            }
+
+            throw new NotSupportedException("Power up not implemented");
+        }
+    }
+}
